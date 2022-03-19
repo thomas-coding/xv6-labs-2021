00635053b65beb362e23686f6ad4825225b6e617
Jinping Wu <jinping.wu@verisilicon.com>
2022-03-19 18:44:44 +0800

    Lab: networking

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -102,7 +102,29 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+
+  acquire(&e1000_lock);
+  uint32 index = regs[E1000_TDT];
+  struct tx_desc *tx_d = &tx_ring[index];
+  if(tx_d->status != E1000_TXD_STAT_DD) {
+    printf("e1000 transmit error \n");
+    release(&e1000_lock);
+    return -1;
+  }
+
+  if(tx_d->addr != 0) {
+    mbuffree(tx_mbufs[index]);
+  }
+
+  tx_mbufs[index] = m;
+
+  tx_d->addr = (uint64)m->head;
+  tx_d->length = m->len;
+  tx_d->cmd |= (E1000_TXD_CMD_RS|E1000_TXD_CMD_EOP);
+
+  regs[E1000_TDT] = (index+1)%TX_RING_SIZE;
+
+  release(&e1000_lock);
   return 0;
 }
 
@@ -115,6 +137,24 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  uint32 index = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+  while(1) {
+      if(rx_ring[index].status & E1000_RXD_STAT_DD) {//have data
+        rx_mbufs[index]->len = rx_ring[index].length;
+        net_rx(rx_mbufs[index]);
+        rx_mbufs[index] = mbufalloc(0);
+        if (!rx_mbufs[index])
+            panic("e1000 111");
+        rx_ring[index].addr = (uint64) rx_mbufs[index]->head;
+        rx_ring[index].status = 0;
+        regs[E1000_RDT] = index;
+        //printf("change tail to tail:%d\n", regs[E1000_RDT]);
+        index++;
+        index = index%RX_RING_SIZE;
+      } else {
+          break;
+      }
+  }
 }
 
 void
